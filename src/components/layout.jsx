2c722129c94b0e59b2e6b6ff02a2d51abadcb3a7
Csizmadia László <csizilaci92@gmail.com>
2022-10-08 09:53:10 +0200

    Add layout component

---
+++ src/components/layout.jsx
@@ -0,0 +1,20 @@
+﻿import {
+    Link,
+    Outlet
+} from "react-router-dom";
+import nasaLogo from "../assets/nasa-logo.png"
+
+const Layout = () => {
+    return (
+        <>
+            <NavigationBar></NavigationBar>
+            <div className={"main-container"}>
+                <Outlet/>
+            </div>
+            <Footer></Footer>
+        </>
+    )
+}
+
+
+export default Layout;
