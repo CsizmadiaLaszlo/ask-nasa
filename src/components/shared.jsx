3d08e2c3fc8795a1bace5fcd563a64b466195561
Csizmadia László <csizilaci92@gmail.com>
2022-10-08 09:50:22 +0200

    Add spinning loading component

---
+++ src/components/shared.jsx
@@ -0,0 +1,18 @@
+﻿export const LoadingSpinner = () => {
+    return (
+        <div className="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
