1a83626aad66464d1ae0def52ac7d27c3883e721
Csizmadia László <csizilaci92@gmail.com>
2022-10-08 09:52:34 +0200

    Add picture of the day component

--- src/components/pictureOfTheDay.jsx
+++ src/components/pictureOfTheDay.jsx
@@ -1,1 +1,44 @@
+﻿import {useContext, useEffect, useState} from "react";
+import {getPictureByDate} from "../services/nasaApiHandler.js";
+import {LoadingSpinner} from "./shared.jsx";
+import PictureContext from "../context/PictureContext.jsx";
 
+const PictureOfTheDay = () => {
+    const {date} = useContext(PictureContext)
+    const [pictureData, setPictureData] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(true);
+        getPictureByDate(date)
+            .then((data) => {
+                setPictureData(data);
+            })
+            .finally(() => {
+                setLoading(false);
+                console.log(pictureData)
+            });
+    }, [date]);
+    return (
+        loading ?
+            <div className={"spinner-container"}>
+                {LoadingSpinner()}
+            </div>
+            :
+            <>
+                <div className={"picture-data-title-container"}>
+                    <h1>{pictureData["title"]}</h1>
+                </div>
+                <div className="picture-data-container">
+                    <div className="picture-details-container">
+                        <h3>Explanation:</h3>
+                        <p className={"picture-explanation"}>{pictureData["explanation"]}</p>
+                    </div>
+                    <Picture url={pictureData["url"]}></Picture>
+                </div>
+            </>
+    )
+}
+
+
+export default PictureOfTheDay
