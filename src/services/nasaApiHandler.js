ce345153c5195db346516a8632355190ebbb25dc
Csizmadia László <csizilaci92@gmail.com>
2022-10-08 09:48:08 +0200

    Add Nasa API handler

---
+++ src/services/nasaApiHandler.js
@@ -0,0 +1,13 @@
+﻿const APIKEY = "REPLACE THIS TEXT WITH YOUR NASA API KEY";
+
+export const getPictureByDate = async (date) => {
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${APIKEY}&date=${date}`;
+    try {
+        const response = await fetch(url);
+        return response.json()
+        
+    }
+    catch {
+        throw new Error('Could not fetch data from the api')
+    }
+}
