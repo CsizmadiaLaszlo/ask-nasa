e587d4031771ac31dc91915fb8c165c20f0e4f99
Csizmadia László <csizilaci92@gmail.com>
2022-10-08 09:49:52 +0200

    Add picture context

---
+++ src/context/PictureContext.jsx
@@ -0,0 +1,8 @@
+﻿import {createContext} from "react";
+
+const PictureContext = createContext({
+    date: new Date().toISOString().slice(0, 10),
+    setDate: () => {}
+});
+
+export default PictureContext
