cb474f257439af42578a89591ce41250b3609c1d
Csizmadia László <csizilaci92@gmail.com>
2022-10-08 11:53:39 +0200

    Add API key check

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -2,11 +2,13 @@ import {useState} from 'react'
 import PictureContext from "../context/PictureContext.jsx";
 import DayChooser from "../components/dayChooser.jsx";
 import PictureOfTheDay from "../components/pictureOfTheDay.jsx";
+import {API_KEY} from "../../config.js";
 
 const Index = () => {
     const [date, setDate] = useState(new Date().toISOString().slice(0, 10))
-    
+    const apiKeyIsSet = API_KEY !== "YOUR API KEY"
     return (
+        apiKeyIsSet ?
         <PictureContext.Provider value={{date, setDate}}>
             <div className="daily-picture-container">
                 <div className="daily-menu">
@@ -23,6 +25,8 @@ const Index = () => {
                 </div>
             </div>
         </PictureContext.Provider>
+            :
+            <h1 style={{textAlign:"center"}}>Please set your api key in config.js</h1>
     )
 }
 
